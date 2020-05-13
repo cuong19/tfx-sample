b789db441bd7638aa4ab2b26d8838b4ee0545b93
cuong19 <cnpgvc@gmail.com>
2020-05-13 10:32:31 +0900

    adder savedmodel

---
+++ adder.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+class Adder(tf.Module):
+
+    @tf.function(input_signature=[tf.TensorSpec(shape=None, dtype=tf.float32)])
+    def add(self, x):
+        return x + x + 1.
+
+
+if __name__ == "__main__":
+    to_export = Adder()
+    tf.saved_model.save(to_export, 'tmp/adder')
