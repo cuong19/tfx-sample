8a4b5eb427b329b98c68e4823348db23930592a5
cuongng <cuong.nguyen@brains-tech.co.jp>
2020-05-20 07:44:36 +0900

    Add gRPC client

--- adder_client.py
+++ adder_client.py
@@ -1,14 +1,17 @@
 import json
 from typing import List
 
+import grpc
 import requests
+import tensorflow as tf
+from tensorflow_serving.apis import prediction_service_pb2_grpc, predict_pb2
 
 
 def rest_infer(instances: List,
                model_name='adder',
                host='localhost',
                port=8501,
-               signature_name="serving_default"
+               signature_name='serving_default'
                ):
     data = json.dumps({
         "signature_name": signature_name,
@@ -17,12 +20,30 @@ def rest_infer(instances: List,
 
     headers = {"content-type": "application/json"}
     json_response = requests.post(
-        'http://{}:{}/v1/models/{}:predict'.format(host, port, model_name),
+        f'http://{host}:{port}/v1/models/{model_name}:predict',
         data=data,
         headers=headers
     )
     print(json_response.json())
 
 
+def grpc_infer(instances: List,
+               model_name='adder',
+               host='localhost',
+               port=8500,
+               signature_name='serving_default'):
+    channel = grpc.insecure_channel(f"{host}:{port}")
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = model_name
+    request.model_spec.signature_name = signature_name
+    request.inputs["x"].CopyFrom(tf.make_tensor_proto(instances))
+    result = stub.Predict(request)
+    result = result.outputs["output_0"]
+    print(result)
+
+
 if __name__ == "__main__":
-    rest_infer([1, 2, 3])
+    val = [1.0, 2.0, 3.0]
+    rest_infer(val)
+    grpc_infer(val)
